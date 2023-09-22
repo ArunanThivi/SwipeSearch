ebbbe935ea512430b234ed4c90c63a4f215c7a2b
Arunan Thiviyanathan <33078790+ArunanThivi@users.noreply.github.com>
2023-09-22 02:44:03 -0700

    Version 2.0
        Revamped Visual Design of all pages (Playlist Page Spotify UI, gradient ground UI)
        Added Ability to Search for and swipe through any playlist
        Added ability to add songs directly to a playlist

--- js/app.js
+++ js/app.js
@@ -11,7 +11,16 @@ const APIController = (function () {
     }
 
     const _getPlaylists = async (token, offset) => {
-        const result = await fetch(`https://api.spotify.com/v1/me/playlists?offset=${offset}`, {
+        const result = await fetch(`https://api.spotify.com/v1/me/playlists?limit=50&offset=${offset}`, {
+            method: 'GET',
+            headers: { 'Authorization': 'Bearer ' + token },
+        });
+        const data = await result.json();
+        return data;
+    }
+
+    const _searchItems = async (token, query, type, offset) => {
+        const result = await fetch(`https://api.spotify.com/v1/search?q=${query}&type=${type}&offset=${offset}`, {
             method: 'GET',
             headers: { 'Authorization': 'Bearer ' + token },
         });
@@ -52,9 +61,16 @@ const APIController = (function () {
             method: 'PUT',
             headers: { 'Authorization': 'Bearer ' + token },
         });
+        return result;
     };
 
-    // const _saveToPlaylist = async (token, playlistId, ids) => {
+    const _saveToPlaylist = async (token, playlistId, ids) => {
+        const result = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks?uris=${ids}`, {
+            method: 'POST',
+            headers: { 'Authorization': 'Bearer ' + token },
+        });
+        return result;
+    };
 
     return {
         getProfile(token) {
@@ -62,6 +78,9 @@ const APIController = (function () {
         },
         getPlaylists(token, offset) {
             return _getPlaylists(token, offset);
+        },
+        searchItems(token, query, type) {
+            return _searchItems(token, query, type, offset);
         },
         getPlaylistSongs(token, playlistId) {
             return _getPlaylistSongs(token, playlistId);
@@ -74,25 +93,29 @@ const APIController = (function () {
         },
         saveSongs(token, ids) {
             return _saveSongs(token, ids);
+        },
+        saveToPlaylist(token, playlistId, ids) {
+            return _saveToPlaylist(token, playlistId, ids);
         }
     }
 
 })();
 
 function getHashParams() {
-    console.log("TEST!")
-    var hashParams = {};
+    var params = {};
+    var searchParams = new URLSearchParams(window.location.search);
     var e, r = /([^&;=]+)=?([^&;]*)/g,
         q = window.location.hash.substring(1);
-      while ( e = r.exec(q)) {
-        hashParams[e[1]] = decodeURIComponent(e[2]);
-      }
-    console.log(hashParams)
-    return hashParams;
-  };
+    while ( e = r.exec(q)) {
+        params[e[1]] = decodeURIComponent(e[2]);
+    }
+    searchParams.forEach((v, k) => params[k] = v);
+    return params;
+};
 
-  function displayPlaylists(playlists, count = 20, start = 0) {
-    console.log(playlists)
+//Playlists.html
+
+function displayPlaylists(playlists, count = 20, start = 0) {
     for (let i = start; i < count; i++) {
         if (playlists[i].images.length < 1){
             continue;
@@ -104,78 +127,49 @@ function getHashParams() {
         var boxElement = `
         <div class='songInfo' id='playlist${i}'>
         <a href='Playlist.html?playlist=${id}'><img src=${cover} class='playlistTile link'></a><br>
-        ${name}<br>
-        By: ${owner}<br>
+        <span class='playlistName'>${name}</span><br>
+        <span class='playlistAuthor'>By: ${owner}</span><br>
         </div>`
         document.getElementById('playlistList').insertAdjacentHTML('beforeend', boxElement);
 
-    
+
     }
-}
-
-
-function showSaved() {
-    document.getElementById("savedSongsList").innerHTML = "";
-    document.getElementById("savedSongs").style.display = "block";
-    var saved = JSON.parse(localStorage["savedSongs"]);
-    for (let i in saved) {
-        var song = saved[i];
-        console.log(song);
-        var songBox = document.createElement("div");
-        songBox.className = "savedInfo";
-        songBox.id = "saved" + i;
-        songBox.dataset.id = i;
-        var cover = document.createElement("img");
-        cover.src = song.album.images[1].url;
-        cover.className = "savedTile";
-        cover.onclick = function () { removedSaved(this.dataset.id) };
-        songBox.appendChild(cover);
-        var songInfo = document.createElement("div");
-        songInfo.className = "savedSongInfo";
-        songInfo.innerHTML += song.name + "<br>";
-        songInfo.innerHTML += song.artists[0].name;
-        songBox.appendChild(songInfo);
-        document.getElementById("savedSongsList").appendChild(songBox);
-    }
-    document.getElementById("savedButton").innerHTML = "&times;"
-    document.getElementById("savedButton").onclick = closeSaved;
-}
-
-function removedSaved(i) {
-    var saved = JSON.parse(localStorage["savedSongs"]);
-    var URIs = new Set(JSON.parse(localStorage["uriSet"]))
-    URIs.delete(saved.splice(i, 1)[0].uri);
-    localStorage["savedSongs"] = JSON.stringify(saved);
-    localStorage["uriSet"] = JSON.stringify(Array.from(URIs));
-    showSaved();
-}
-
-function closeSaved() {
-    var saved = JSON.parse(localStorage["savedSongs"]);
-    document.getElementById("savedButton").innerText = saved.length;
-    document.getElementById("savedButton").onclick = showSaved;    document.getElementById("savedSongs").style.display = "none";
 }
 
 function handleScroll() {
     const scrollHeight = document.documentElement.scrollHeight;
     const scrollTop = document.documentElement.scrollTop;
     const clientHeight = document.documentElement.clientHeight;
-
     if (scrollHeight - scrollTop - clientHeight < 200) {
-        APIController.getPlaylists(access_token, offset).then(playlists => {
+        if (term) {
+            APIController.searchItems(access_token, term, 'playlist', offset).then(response => {
+                // Process the fetched playlists
+                playlists = response.playlists;
+                offset += 50;
+                displayPlaylists(playlists.items, playlists.items.length, 0);
+                if (playlists.next == null) {
+                    document.removeEventListener('scroll', handleScroll);
+                }
+            })
+            .catch(error => {
+                // Handle any errors that occur during the API call
+                console.error('Error fetching playlists:', error);
+            });
+        } else {
+            APIController.getPlaylists(access_token, offset).then(playlists => {
             // Process the fetched playlists
-            offset += 20;
-            console.log('Fetched playlists:', playlists);
-            displayPlaylists(playlists.items, playlists.items.length, 0);
-            if (playlists.next == null) {
-                document.removeEventListener('scroll', handleScroll);
-            }
-        })
-        .catch(error => {
-            // Handle any errors that occur during the API call
-            console.error('Error fetching playlists:', error);
-            console.log("token " + access_token)
-        });
+                offset += 50;
+                displayPlaylists(playlists.items, playlists.items.length, 0);
+                if (playlists.next == null) {
+                    document.removeEventListener('scroll', handleScroll);
+                }
+            })
+            .catch(error => {
+                // Handle any errors that occur during the API call
+                console.error('Error fetching playlists:', error);
+            });
+        }
+        
     }
 }
 
@@ -189,4 +183,149 @@ function debounce(func, delay) {
         func.apply(this, args);
       }, delay);
     };
-  }
+}
+
+//Playlist.html
+
+function setCard(song) {
+    $('#dummyCard').html($('#songInfo').html())
+    document.getElementById('cover').src = song.album.images[0].url;
+    document.getElementById('title').innerHTML = song.name;
+    document.getElementById('artist').innerHTML = song.artists.map(obj => obj.name).join(', ');
+    document.getElementById('preview').src = song.preview_url;
+    var img = $('#cover');
+    img.imgcolr(function (img, color) {
+        const newBG = `linear-gradient(0deg, ${color} 0%, ${color} 100%), linear-gradient(180deg, rgba(0, 0, 0, 0.44) 39%, rgba(0, 0, 0, 0.88) 100%)`;
+        $("body").css({background:newBG});
+    });
+}
+
+function swipeSong(i, songs, save) {
+    
+    if (save) {
+        addLiked(songs[i])
+    }
+    i++;
+    if (i >= songs.length) {
+        //Hide Card Element
+        card.style.display = 'none';
+        $('#completed').show();
+    } else {
+        setCard(songs[i]);
+    }
+    
+    dummy.style.transform = 'translateX(' + (save ? '100%' : '-200%') + ')';
+    dummy.style.transition = 'transform 0.5s ease-out';
+    
+    setTimeout(() => {
+        dummy.style.transform = '';
+        dummy.style.transition = '';
+        dummy.innerHTML = "";
+    }, 300);
+    return i;
+}
+
+function undoSwipe(i, songs) {
+    card.style.transition = '';
+    i = Math.max(i-1, 0);
+    card.style.left = '-100%'
+    
+    setTimeout(() => {
+        setCard(songs[i]);
+        card.style.transition = 'left 0.5s ease-in';
+        card.style.left = '50%'
+        dummy.innerHTML = "";
+    }, 0);
+
+    return i;
+}
+
+function showLiked() {
+    $('#main').addClass('blur');
+    $('#overlayTwoHide').hide();
+    document.getElementById("savedSongsList").innerHTML = "";
+    $('.saveButton').show();
+    document.getElementById('overlay').classList.add('active');
+    document.getElementById('overlayExit').style.display = 'block';
+    // $('.saveButton').css('display', 'inline-block');
+    var saved = localStorage["savedSongs"] ? JSON.parse(localStorage["savedSongs"]) : {};
+    for (let i in saved) {
+        var song = saved[i];
+        var songBox = document.createElement("div");
+        songBox.className = "savedInfo";
+        songBox.id = "saved" + i;
+        var cover = document.createElement("img");
+        cover.src = song.album.images[1].url;
+        cover.className = "savedTile";
+        cover.dataset.id = i;
+        cover.onclick = function () { removeLiked(this) };
+        songBox.appendChild(cover);
+        var songInfo = document.createElement("div");
+        songInfo.className = "savedSongInfo";
+        songInfo.innerHTML += song.name + "<br>";
+        songInfo.innerHTML += song.artists[0].name;
+        songBox.appendChild(songInfo);
+        document.getElementById("savedSongsList").appendChild(songBox);
+    }
+    document.getElementById("overlayExit").onclick = closeLiked;
+}
+
+function showPlaylists() {
+    const overlay = $('#overlayTwo')
+    document.getElementById('playlistList').innerText = ''
+    overlay.toggleClass('active');
+    $('#overlayTwoHide').show();
+    var access_token = localStorage["token"];
+    APIController.getPlaylists(access_token, 0).then((promise) => {
+        APIController.getProfile(access_token).then(({id}) => {
+            const playlists = promise.items.filter((item) => item.owner.id === id);
+            for (let i in playlists) {
+                var item = playlists[i];
+                var box = document.createElement("div");
+                box.innerHTML += item.name;
+                box.dataset.id = item.id;
+                box.onclick = function() { saveToPlaylist(this) };
+                document.getElementById('playlistList').appendChild(box);
+            }
+        })
+
+    })
+    
+}
+
+function saveToPlaylist(song) {
+    const access_token = localStorage['token']
+    const id = song.dataset.id
+    const URIs = JSON.parse(localStorage['uriSet']).join(',')
+    APIController.saveToPlaylist(access_token, id, URIs)
+}
+
+function addLiked(song) {
+    if (!uriSet.has(song.uri)) {
+        uriSet.add(song.uri);
+        savedSongs.push(song);
+        localStorage['savedSongs'] = JSON.stringify(savedSongs);
+        localStorage['uriSet'] = JSON.stringify(Array.from(uriSet));
+    }
+}
+function removeLiked(song) {
+    const i = song.dataset.id;
+    uriSet.delete(savedSongs.splice(i, 1)[0].uri);
+    localStorage["savedSongs"] = JSON.stringify(savedSongs);
+    localStorage["uriSet"] = JSON.stringify(Array.from(uriSet));
+    showLiked();
+}
+
+function saveLikes() {
+    var songIds = savedSongs.map(obj => obj.id).join(',');
+    var access_token = localStorage["token"];
+    APIController.saveSongs(access_token, songIds);
+}
+
+function closeLiked() {
+    $('#main').removeClass('blur');
+    document.getElementById("openOverlay").onclick = showLiked;
+    $('.saveButton').hide();
+    document.getElementById('overlay').classList.remove('active');
+    $("#overlayExit").hide();
+}
